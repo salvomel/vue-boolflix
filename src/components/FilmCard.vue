ab3f05290a86c7737d04d1ddc4d4b2a0b0675e3a
salvomel <salv.mel@gmail.com>
2022-01-12 15:27:14 +0100

    milestone-3 posters

--- src/components/FilmCard.vue
+++ src/components/FilmCard.vue
@@ -1,21 +1,27 @@
 <template>
     <div class="single-card">
-        <div>
-            <span class="title">Titolo: {{filmInfo.title}}</span>
+        <div class="poster" >
+            <img :src="`https://image.tmdb.org/t/p/w185/${filmInfo.poster_path}`" :alt="filmInfo.title">
         </div>
-        <div>
-            <span class="original-title">Titolo originale: {{filmInfo.original_title}}</span>
+        <div class="title">
+            <h5>Titolo:</h5>
+            <h4>{{filmInfo.title}}</h4>
         </div>
-        <div>
-            <span v-if="filmInfo.original_language === 'en'" class="language">Lingua: <img src="../assets/img/en.png" alt="en"></span>
-            <span v-else-if="filmInfo.original_language === 'it'" class="language">Lingua: <img src="../assets/img/it.png" alt="it"></span>
-            <span v-else-if="filmInfo.original_language === 'fr'" class="language">Lingua: <img src="../assets/img/fr.png" alt="fr"></span>
-            <span v-else-if="filmInfo.original_language === 'es'" class="language">Lingua: <img src="../assets/img/es.png" alt="es"></span>
-            <span v-else-if="filmInfo.original_language === 'de'" class="language">Lingua: <img src="../assets/img/de.png" alt="de"></span> 
-            <span v-else class="language">Lingua: {{filmInfo.original_language}}</span>    
+        <div class="original-title">
+            <h5>Titolo originale:</h5>
+            <h4>{{filmInfo.original_title}}</h4>
         </div>
-        <div>
-            <span class="vote">Voto: {{filmInfo.vote_average}}</span>
+        <div class="language">
+            <span v-if="filmInfo.original_language === 'en'">Lingua: <img src="../assets/img/en.png" alt="en"></span>
+            <span v-else-if="filmInfo.original_language === 'it'">Lingua: <img src="../assets/img/it.png" alt="it"></span>
+            <span v-else-if="filmInfo.original_language === 'fr'">Lingua: <img src="../assets/img/fr.png" alt="fr"></span>
+            <span v-else-if="filmInfo.original_language === 'es'">Lingua: <img src="../assets/img/es.png" alt="es"></span>
+            <span v-else-if="filmInfo.original_language === 'de'">Lingua: <img src="../assets/img/de.png" alt="de"></span> 
+            <span v-else>Lingua: {{filmInfo.original_language}}</span>    
+        </div>
+        <div class="vote">
+            <h5>Voto:</h5>
+            <h4>{{filmInfo.vote_average}}</h4>
         </div>
     </div>
 </template>
--- src/components/SeriesCard.vue
+++ src/components/SeriesCard.vue
@@ -1,21 +1,27 @@
 <template>
     <div class="single-card">
-        <div>
-            <span class="title">Nome: {{seriesInfo.name}}</span>
+        <div class="poster" >
+            <img :src="`https://image.tmdb.org/t/p/w185/${seriesInfo.poster_path}`" :alt="seriesInfo.title">
         </div>
-        <div>
-            <span class="original-title">Nome originale: {{seriesInfo.original_name}}</span>
+        <div class="title">
+            <h5>Nome:</h5>
+            <h4>{{seriesInfo.name}}</h4>
         </div>
-        <div>
-            <span v-if="seriesInfo.original_language === 'en'" class="language">Lingua: <img src="../assets/img/en.png" alt="en"></span>
-            <span v-else-if="seriesInfo.original_language === 'it'" class="language">Lingua: <img src="../assets/img/it.png" alt="it"></span>
-            <span v-else-if="seriesInfo.original_language === 'fr'" class="language">Lingua: <img src="../assets/img/fr.png" alt="fr"></span>
-            <span v-else-if="seriesInfo.original_language === 'es'" class="language">Lingua: <img src="../assets/img/es.png" alt="es"></span>
-            <span v-else-if="seriesInfo.original_language === 'de'" class="language">Lingua: <img src="../assets/img/de.png" alt="de"></span> 
-            <span v-else class="language">Lingua: {{seriesInfo.original_language}}</span>    
+        <div class="original-title">
+            <h5>Nome originale:</h5>
+            <h4>{{seriesInfo.original_name}}</h4>
         </div>
-        <div>
-            <span class="vote">Voto: {{seriesInfo.vote_average}}</span>
+        <div class="language">
+            <span v-if="seriesInfo.original_language === 'en'">Lingua: <img src="../assets/img/en.png" alt="en"></span>
+            <span v-else-if="seriesInfo.original_language === 'it'">Lingua: <img src="../assets/img/it.png" alt="it"></span>
+            <span v-else-if="seriesInfo.original_language === 'fr'">Lingua: <img src="../assets/img/fr.png" alt="fr"></span>
+            <span v-else-if="seriesInfo.original_language === 'es'">Lingua: <img src="../assets/img/es.png" alt="es"></span>
+            <span v-else-if="seriesInfo.original_language === 'de'">Lingua: <img src="../assets/img/de.png" alt="de"></span> 
+            <span v-else>Lingua: {{seriesInfo.original_language}}</span>    
+        </div>
+        <div class="vote">
+            <h5>Voto:</h5>
+            <h4>{{seriesInfo.vote_average}}</h4>
         </div>
     </div>
 </template>
