8aa56d158827d4a7ebd0d09e30d86566192bbacc
salvomel <salv.mel@gmail.com>
2022-03-05 23:09:55 +0100

    fixes

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,6 @@
     <header>
         <div class="logo">
             <img src="../assets/img/logo.jpg" alt="Logo">
-            <h1>BOOLFLIX</h1>
         </div>
         <div>
             <input type="text" placeholder="Cerca un film o una serie TV" v-model="searchText">
